7085ca7ae3de68c65d44d8b7095926921467f0c6
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-20 15:00:52 -0400

    dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,3 +44,60 @@ router.get("/login", (req, res) => {
     res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("signup");
+});
+
+router.get("/post/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "created_at",
+            "post_content"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username", "twitter", "github"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username", "twitter", "github"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post with this ID" });
+            return;
+        }
+
+        // Serialize the data
+        const post = dbPostData.get({ plain: true });
+
+        // Pass the data to a template
+        res.render("single-post", {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
