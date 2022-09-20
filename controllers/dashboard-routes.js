f979a3afcb4c14a2046cc6983f7475e7d7e4f143
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-20 16:06:14 -0400

    commit dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -86,3 +86,43 @@ router.get("/edit/:id", withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get("/create/", withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // Use the session ID
+            user_id: req.session.user_id
+        },
+        attributes: [
+            "id",
+            "title",
+            "created_at",
+            "post_content"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username", "twitter", "github"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username", "twitter", "github"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // Serialize the data
+        const posts = dbPostData.map(post => post.get({ plain:true}));
+        res.render("create-post", { post, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
