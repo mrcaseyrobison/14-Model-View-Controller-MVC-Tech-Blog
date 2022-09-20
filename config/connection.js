b344119870355b11ea961a4042fb0f5b8f6bf8c9
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-20 14:22:04 -0400

    database schema

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 // Require Sequelize //
 const Sequelize = require("sequelize");
-
 require("dotenv").config();
 
 // Connect to the database //
@@ -9,7 +8,11 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_PW, {
+    sequelize = new Sequelize(
+        process.env.DB_NAME, 
+        process.env.DB_USER,
+        process.env.DB_PW, 
+        {
         host: "localhost",
         dialect: "mysql",
         port: 3306
