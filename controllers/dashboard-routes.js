a0b589584f59249ca7552b7e443663c5bdef4d6c
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-20 15:38:35 -0400

    working on it

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -24,6 +24,10 @@ router.get("/", withAuth, (req, res) => {
                     attributes: ["username", "twitter", "github"]
                 }
             },
+            {
+                model: User,
+                attributes: ["username", "twitter", "github"]
+            }
         ]
     })
     .then(dbPostData => {
@@ -35,4 +39,50 @@ router.get("/", withAuth, (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
-})
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "created_at",
+            "post_content"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "common_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username", "twitter", "github"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username", "twitter", "github"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post with this ID"});
+            return;
+        }
+
+        // Serialize the data
+        const post = dbPostData.get({ plain: true});
+
+        res.render("edit-post", {
+            post,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
