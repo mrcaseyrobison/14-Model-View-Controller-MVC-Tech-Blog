5b9eeba5bb69e7f93e207dabf201dc2eb25bf63e
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-22 16:12:16 -0400

    user-routes untested

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,4 +102,57 @@ router.post("/login", (req, res) => {
     });
 });
 
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+// PUT API/users/id
+router.put("/:id", withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: "No user with this ID" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.llg(err);
+        res.status(500),json(err);
+    });
+});
+
+// DELETE API/users/id
+router.delete("/:id", withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: "No user with this ID" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
 
