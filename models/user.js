39da3bed480febfd36fc80b117987d92d13249fb
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-23 16:21:35 -0400

    user model

---
+++ models/user.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require("serialize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+// Creates the User Model
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+// Defines columns and confirguration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        twitter: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        github: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
+        sequelize,
+        timestaps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user"
+    }
+);
+
+module.exports = User;
